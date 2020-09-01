2c4f4f0ad5115cd85b631e78ea3b82aaa82119e0
Yizhen Liu <imliuyzh@outlook.com>
2020-09-01 11:34:34 -0700

    Completed the front-end for PC devices

---
+++ scripts/index.js
@@ -0,0 +1,4 @@
+window.onload = function () {
+  let imageUploadSection = document.querySelector("#image-upload-section");
+  imageUploadSection.addEventListener("click", () => document.querySelector("#upload-prompt").click());
+};
