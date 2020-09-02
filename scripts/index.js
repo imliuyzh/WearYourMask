68f5848a1ffa49b54c51cfa701ed09a208051b8c
Yizhen Liu <imliuyzh@outlook.com>
2020-09-02 11:46:02 -0700

    Added a background picture

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,45 @@
 window.onload = async function () {
+  let mobileNetModule = await mobilenet.load({ version: 2, alpha: 1 }),
+  classifier = await trainModel(mobileNetModule);
+
   let imageUploadSection = document.querySelector("#image-upload-section");
   imageUploadSection.addEventListener("click", () => document.querySelector("#upload-prompt").click());
-
-  let mobileNetModule = await mobilenet.load({ version: 2, alpha: 1 });
-  let classifier = await trainModel(mobileNetModule);
-  let selectedImage = tf.browser.fromPixels();
+  imageUploadSection.addEventListener("change", async (event) => {
+    console.log(event);
+    let tfTestImage = tf.browser.fromPixels(testImage);
+    let logits = mobilenetModule.infer(tfTestImage, 'conv_preds');
+    let prediction = await classifier.predictClass(logits);
+    console.log(prediction);
+  });
 };
 
 async function trainModel(mobileNetModule) {
   let classifier = knnClassifier.create();
-  let maskImages = [], noMaskImages = [];
+  let [maskImages, noMaskImages] = addImages();
+  maskImages.forEach(image =>
+    classifier.addExample(mobileNetModule.infer(tf.browser.fromPixels(image), "conv_preds"), 1)
+  );
+  noMaskImages.forEach(image => 
+    classifier.addExample(mobileNetModule.infer(tf.browser.fromPixels(image), "conv_preds"), 0)
+  );
+  return classifier;
+}
 
-  for (let counter = 0; counter < 20; ++counter) {
+function addImages() {
+  let maskImages = [], noMaskImages = [];
+  for (let counter = 1; counter <= 20; ++counter) {
     let maskImage = document.createElement("img"),
         noMaskImage = document.createElement("img");
 
     maskImage.setAttribute("src", `assets/training-samples/mask/${counter}.jpg`);
-    noMaskImage.setAttribute("src", `assets/training-samples/no-mask/${counter}.jpg`);
-
+    maskImage.setAttribute("width", "100%");
+    maskImage.setAttribute("height", "100%");
     maskImages.push(maskImage);
+
+    noMaskImage.setAttribute("src", `assets/training-samples/no-mask/${counter}.jpg`);
+    noMaskImage.setAttribute("width", "100%");
+    noMaskImage.setAttribute("height", "100%");
     noMaskImages.push(noMaskImage);
   }
-
-  maskImages.forEach(image =>
-    classifier.addExample(mobilenetModule.infer(tf.browser.fromPixels(image), "conv_preds"), 1)
-  );
-
-  noMaskImages.forEach(image => 
-    classifier.addExample(mobilenetModule.infer(tf.browser.fromPixels(image), "conv_preds"), 0)
-  );
-
-  return classifier;
+  return [maskImages, noMaskImages];
 }
